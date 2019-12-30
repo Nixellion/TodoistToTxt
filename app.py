9427a1dfb1586c76651e854caf4b4d07d1592715
ShadeAnimator <nixesvfx@gmail.com>
2019-12-30 16:09:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,31 @@ import yaml
 import todoist
 import string
 
+# easywebdav python3 hack
 import easywebdav.client
-
 easywebdav.basestring = str
 easywebdav.client.basestring = str
+
+
 import easywebdav
 
-from pprint import PrettyPrinter
-
-pp = PrettyPrinter(indent=4)
+#from pprint import PrettyPrinter
+#pp = PrettyPrinter(indent=4)
 
 appdir = os.path.dirname(os.path.realpath(__file__))  # rp, realpath
 
+
+
+# Read config
 with open(os.path.join(appdir, 'config.yaml'), 'r') as f:
     config = yaml.load(f.read())
 
+
+# Fetch todoist items
 api = todoist.TodoistAPI(config['todoist_token'])
 api.sync()
+
+# Pprint for debug purposes
 # pp.pprint(api.state)
 
 # Find required project
@@ -30,19 +38,19 @@ for key, value in api.state.items():
             if project['name'] == config['todoist_project']:
                 project_id = project['id']
                 break
-
         break
 
-items = []
 
+# Filter items
+items = []
 for value in api.state['items']:
     if value['project_id'] == project_id:
         if config['max_items'] != 0 and len(items) > int(config['max_items']) - 1:
             break
         items.append("({}) {}".format(string.ascii_uppercase[4 - value['priority']], value['content']))
 
+# Create output text
 output_text = ""
-
 for i in sorted(items):
     # print (i)
     output_text += i + "\n"
@@ -50,10 +58,12 @@ for i in sorted(items):
 # TODO Make 'type' selection work
 local_filepath = os.path.join(appdir, config['filename_output'])
 
+# Write to local file
 with open(local_filepath, 'w+', encoding='utf-8', errors='ignore') as f:
     f.write(output_text)
 
+# Upload to webdav
 webdav = easywebdav.connect(config['webdav_url'], username=config['webdav_login'], password=config['webdav_password'],
                             protocol='https', port=443, verify_ssl=False, path=config['webdav_path'])
-#print(webdav.ls("/"))
+
 webdav.upload(local_filepath, "{}/{}".format(config['webdav_directory'], config['filename_output']))
