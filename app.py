c53901d8f1431d60bf134fdd996d17116b4ab5a5
Nixellion <nixesvfx@gmail.com>
2020-07-28 18:29:00 +0300

    Added auto clean up of completed tasks, done tasks counter

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import json
 import todoist
 import string
 import tempfile
@@ -10,13 +11,16 @@ from datetime import date, datetime
 
 import pprint
 
-
 easywebdav.basestring = str
 easywebdav.client.basestring = str
 
 import easywebdav
 
 appdir = os.path.dirname(os.path.realpath(__file__))  # rp, realpath
+datadir = os.path.join(appdir, "data")
+
+if not os.path.exists(datadir):
+    os.makedirs(datadir)
 
 # Read config
 with open(os.path.join(appdir, 'config.yaml'), 'r') as f:
@@ -30,16 +34,51 @@ if config['debug']:
     with open(os.path.join(appdir, "debug.json"), "w+", encoding="utf-8") as f:
         f.write(pprint.pformat(api.state, indent=4))
 
+
 def debug(text):
     if config['debug']:
         print(text)
 
-#json.dumps(api.state, default=lambda o: '<not serializable>', indent=4)
+
+# json.dumps(api.state, default=lambda o: '<not serializable>', indent=4)
 # Pprint for debug purposes
 # from pprint import PrettyPrinter
 # pp = PrettyPrinter(indent=4, width=10000)
 # pp.pprint(api.state)
 # input()
+
+today_path = os.path.join(datadir, datetime.now().strftime("%Y_%m_%d") + ".mem")
+
+if not os.path.exists(today_path):
+    with open(today_path, "w+", encoding="utf-8") as f:
+        f.write("")
+
+
+def today_memory():
+    with open(today_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    return lines
+
+
+def remember_task(task):
+    lines = today_memory()
+    if task not in lines:
+        with open(today_path, "a+", encoding="utf-8") as f:
+            f.write(task + "\n")
+        print(f"Task '{task}' added to memory for today.")
+    else:
+        print(f"Task '{task}' already remembered as completed for today.")
+
+
+def completed_today():
+    with open(today_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    tasks = 0
+    for line in lines:
+        if line.strip():
+            tasks += 1
+    return tasks
+
 
 def get_project_id(name):
     project_id = None
@@ -53,6 +92,7 @@ def get_project_id(name):
                     break
             break
     return project_id
+
 
 def get_project_items(project_name):
     # Filter items
@@ -82,11 +122,18 @@ def get_project_items(project_name):
                     item_text = f"{item_text} due:{item['due']['date']}"
                 items.append(item_text)
             elif item['checked'] == 1:
+                remember_task(item['content'])
                 if config['show_completed_tasks']:
                     items.append("x ({}) {}".format(string.ascii_uppercase[4 - item['priority']], item['content']))
+                if config['clean_up_completed_tasks']:
+                    print(f"Deleting task '{item['content']}'")
+                    task = api.items.get_by_id(item['id'])
+                    task.delete()
+                    api.commit()
             else:
                 print("Something's not right, did Todoist change API? item['checked'] is not 0 or 1.")
     return items
+
 
 def text_from_items(items, filter_out=[]):
     # Create output text
@@ -97,16 +144,18 @@ def text_from_items(items, filter_out=[]):
             output_text += i + "\n"
     return output_text
 
+
 def generate_output_text():
     project_items = get_project_items(config['todoist_project'])
     output_text = text_from_items(project_items)
+    inbox_text = text_from_items(get_project_items("Inbox"), filter_out=project_items)
     if config['todoist_append_inbox']:
-        output_text = f'TODAY:\n\n{output_text}\n\nINBOX:\n\n{text_from_items(get_project_items("Inbox"), filter_out=project_items)}'
+        output_text = f'TODAY (Done: {completed_today()}):\n\n{output_text}\n\nINBOX:\n\n{inbox_text}'
     return output_text
+
 
 if __name__ == "__main__":
     # TODO Make 'type' selection work
-
 
     local_filepath = os.path.join(appdir, config['filename_output'])
 
@@ -146,8 +195,6 @@ if __name__ == "__main__":
         with tempfile.NamedTemporaryFile(mode="w+", encoding='utf-8', delete=False) as tmp:
             tmp.write(output_text)
             tmp_fp = tmp.name
-
-
 
         webdav.upload(tmp_fp, "{}/{}".format(config['webdav_directory'], config['filename_output']))
         os.remove(tmp_fp)
